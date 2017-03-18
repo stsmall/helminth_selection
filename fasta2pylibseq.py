9d15a66704143a91767f9b2ff3803d81ec23ac5c
smalls <stsmall@gmail.com>
2017-03-18 17:46:19 -0400

    update

--- fasta2pylibseq.py
+++ fasta2pylibseq.py
@@ -11,28 +11,27 @@ import pickle
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('INfasta', metavar="INfasta",type=str,help='path to fasta file')
-parser.add_argument('-s', '--seqs', type=int, required=True, help="number of sequences")
-parser.add_argument('-l',"--seqlength", type=int, required=True, help="length of sequence")
+parser.add_argument('INfasta', metavar="INfasta", type=str,
+                    help='path to fasta file')
+parser.add_argument('-s', '--seqs', type=int, required=True,
+                    help="number of sequences")
+parser.add_argument('-l', "--seqlength", type=int, required=True,
+                    help="length of sequence")
+parser.add_argument('-p', '--popsizelist', nargs='+', type=int, required=True,
+                    help="population list")
 args = parser.parse_args()
 
-class MSgenotype(object):
-    def __init__(self, haplotypes=None, positions=None,
-            names = None):
-        if haplotypes:
-            self.haps = haplotypes
-        else:
-            self.haps= {}
-        if positions:
-            self.pos = positions
-        else:
-            self.pos = {}
-        if names:
-            self.seq = names
-        else:
-            self.seq = {}
 
-def fasta2pylibseq(fastaFile, numberseqs, lensequence):
+class MSgenotype(object):
+    def __init__(self, haplotype=[], positions=[], names=[], popsizelist=[]):
+
+        self.haps = haplotype
+        self.pos = positions
+        self.names = names
+        self.pops = popsizelist
+
+
+def fasta2pylibseq(fastaFile, numberseqs, lensequence, popsizelist):
     '''take a fasta alignmnet with out group as first sequence and convert it
     to ms-type format.
 
@@ -41,7 +40,7 @@ def fasta2pylibseq(fastaFile, numberseqs, lensequence):
     fasta : file
         alignment in fasta format
     numberseq : int
-        number of sequences to expect for array size
+        number of sequences to expect for array size not including reference
     lensequence : int
         length of sequence
     Returns
@@ -52,10 +51,10 @@ def fasta2pylibseq(fastaFile, numberseqs, lensequence):
         list of mutation positions 0 - 1
     '''
     seqnames = []
-    positions = np.arange(1, lensequence + 1) / float(lensequence)
+    positions = np.arange(0.0, 1.0, 1.0/lensequence)
     genotypes = np.zeros(shape=[numberseqs, len(positions)])
     tmpfasta = []
-    with open(fastaFile, 'r') as fasta_aln:
+    with open("{}".format(fastaFile), 'r') as fasta_aln:
         for line in fasta_aln:
             if line.startswith(">"):
                 seqnames.append(line[1:].strip())
@@ -64,21 +63,22 @@ def fasta2pylibseq(fastaFile, numberseqs, lensequence):
 
     refseq = tmpfasta[0]
     for ind, seq in enumerate(tmpfasta[1:]):
-        muts = [i for i in xrange(len(refseq)) if refseq[i] != seq[i]]
+        try:
+            muts = [i for i in range(len(seq)) if refseq[i] != seq[i]]
+        except IndexError:
+            pass
         genotypes[ind][muts] = 1
 
-    fastaname = ''
-    fastaname= MSgenotype(fastaname)
-    fastaname.haps = genotypes
-    fastaname.pos = positions
-    fastaname.seq = seqnames
+    fastaname = MSgenotype(genotypes, positions, seqnames[1:], popsizelist)
 
     with open('{}.pkl'.format('fastaout'), 'wb') as output:
         pickler = pickle.Pickler(output, -1)
-        fastaout = MSgenotype(fastaname.haps, fastaname.pos, fastaname.seq)
+        fastaout = MSgenotype(fastaname.haps, fastaname.pos, fastaname.names,
+                              fastaname.pops)
         pickler.dump(fastaout)
+    del fastaout
 
     return(None)
 
 if __name__ == '__main__':
-     fasta2pylibseq(args.INfasta, args.seqs, args.seqlength)
+    fasta2pylibseq(args.INfasta, args.seqs, args.seqlength, args.popsizelist)
--- ms2stats.py
+++ ms2stats.py
@@ -5,26 +5,32 @@ Created on Mon Mar 13 16:43:08 2017
 
 @author: scott t small
 """
+import pickle
 import numpy as np
 import pandas as pd
 from itertools import groupby, combinations
 import argparse
 from libsequence.polytable import simData
 from libsequence.summstats import garudStats
-#from libsequence.summstats import polySIM
-#from libsequence.summstats import ld
-#from libsequence.summstats import lhaf
-#from libsequence.windows import Windows
+# from libsequence.summstats import polySIM
+# from libsequence.summstats import ld
+# from libsequence.summstats import lhaf
+# from libsequence.windows import Windows
 
 parser = argparse.ArgumentParser()
-parser.add_argument('INmsfile', metavar="INmsfile",type=str,help='path to ms-formatted file')
-parser.add_argument('-p', '--populations', nargs='+', type=int, required=True, help="population list")
-parser.add_argument('-r',"--reps", type=int, required=True, help="number of reps")
-parser.add_argument('-u',"--priors", type=str, required=True, help="file with priors")
+parser.add_argument('INmsfile', metavar="INmsfile", type=str,
+                    help='path to ms-formatted file')
+parser.add_argument('-p', '--populations', nargs='+', type=int, required=True,
+                    help="population list")
+parser.add_argument('-r', "--reps", type=int, required=True,
+                    help="number of reps")
+parser.add_argument('-u', "--priors", type=str, required=True,
+                    help="file with priors")
 args = parser.parse_args()
 
+
 def site_freqspec_fx(gtarray, pos):
-    '''calculates the site frequency spectrum. If >1 pop also does jSFS
+    """calculates the site frequency spectrum. If >1 pop also does jSFS
 
     Parameters
     ---------
@@ -32,37 +38,46 @@ def site_freqspec_fx(gtarray, pos):
     Returns
     -------
 
-    '''
-    #calculate SFS
-#    sfs_t = np.zeros(len(gtlist),len(gtlist[0]))
-    #gtarray = np.array(map(lambda x: map(int, list(x)), gtlist))
+    """
+    # calculate SFS
     freqsum = np.sum(gtarray, axis=0)
     nind = gtarray.shape[0]
-    #sfs
-    sfsfreq, sfscount = np.unique(np.sort(freqsum), return_counts = True)
+    # sfs
+    sfsfreq, sfscount = np.unique(np.sort(freqsum), return_counts=True)
     sfs = np.zeros(gtarray.shape[0])
     sfs[sfsfreq - 1] = sfscount
 
-    #derived allele freq
-    derived = freqsum[len(freqsum)/2.0] / nind
-    
-    return(sfs,derived)
+    # derived allele freq
+    derived = freqsum[np.int(len(freqsum)/(1.0/pos))] / nind
+
+    return(sfs, derived)
+
+
+def hapconfigfx(config):
+    """
+    """
+    uniqhaps = np.array([np.array(x) for x in set(tuple(x) for x in config)])
+    hapfreq = np.array([len(config[np.all(config == x, axis=1)])
+                        for x in uniqhaps], dtype=int)
+    return(uniqhaps, hapfreq)
+
 
 def jointsfs_fx(gtdict, popiix):
-    '''makes the joint site frequency spectrum
-    
-    '''    
+    """makes the joint site frequency spectrum
+
+    """
     for rep in gtdict.keys():
-        freqsumlist = [np.sum(gtdict[rep][pop]) for pop in popiix]        
-        
-        joint = [(freqsumlist[i][site],freqsumlist[j][site])
+        freqsumlist = [np.sum(gtdict[rep][pop]) for pop in popiix]
+
+        joint = [(freqsumlist[i][site], freqsumlist[j][site])
                  for i, j in combinations(range(len(freqsumlist)), 2)
-                 for site in range(len(freqsumlist[0]))]         
-        jointsfs = [(key,len(list(group))) for key, group in groupby(joint)]
-    return(jointsfs)                
-                    
+                 for site in range(len(freqsumlist[0]))]
+        jointsfs = [(key, len(list(group))) for key, group in groupby(joint)]
+    return(jointsfs)
+
+
 def haploconfig_fx(gtarray):
-    '''construct haplotype configuration array
+    """construct haplotype configuration array
 
     Parameters
     ---------
@@ -71,26 +86,26 @@ def haploconfig_fx(gtarray):
     Returns
     -------
 
-    '''
-    #haplotype configuration from gtarray
-    uniqhaps = np.array([np.array(x) for x in set(tuple(x) for x in gtarray)])
-    hapfreq = np.array([len(gtarray[np.all(gtarray==x, axis=1)]) for x in uniqhaps],dtype=int)
+    """
+    # haplotype configuration from gtarray
+    uniqhaps, hapfreq = hapconfigfx(gtarray)
     n = sum(hapfreq)
     C_freq, C_count = np.unique(hapfreq, return_counts=True)
     C = np.zeros(n)
-    C[C_freq - 1] = C_count #full hap configuration
-    #haplotype diversity
+    C[C_freq - 1] = C_count  # full hap configuration
+    # haplotype diversity
 #    H = 1 - sum([(((i+1)/float(n))**2) * c for i,c in enumerate(C_v)])
 #    M = max(C_v)
 #    K = sum(C_v)
 
-    #from gtlist
-    #K = set(gtlist)
+    # from gtlist
+    # K = set(gtlist)
 
     return(C)
 
+
 def calc_otherstats(positions, gtdict, popsizelist):
-    '''calculate stats from ms-type file
+    """calculate stats from ms-type file
 
     Parameters
     ----------
@@ -103,61 +118,69 @@ def calc_otherstats(positions, gtdict, popsizelist):
     -------
     ms_otherstats : pandas df
 
-    '''
-    #numpops = len(gtdict.keys())
-    garudH12 = []
-    garudH1 = []
-    garudH2 = []
+    """
+    hapconfig_common = []
+    gH12 = []
+    gH1 = []
+    gH21 = []
     sfs = []
-    hapconfig = []
-    derivedfreq = []
+    hap = []
+    dfreq = []
     popiix = []
     ix = 0
-    pos = .5 #need -Sp 0.5 and -Smark
+    pos = 0.5  # need -Sp 0.5 and -Smark
     for pop in popsizelist:
         popiix.append(range(ix, ix + pop))
         ix += pop
-        
-    for rep in gtdict.keys():
-        derivedfreq_t = []
-        sfs_t = np.array([])
-        hapconfig_t = []
-        garudH12_t = []
-        garudH1_t = []
-        garudH2_t = []
+    for rep in range(len(gtdict.keys())):
+        dfreqt = []
+        sfst = []
+        hapt = []
+        gH12t = []
+        gH1t = []
+        gH21t = []
         for iix in popiix:
-            gtarray = gtdict[rep][iix]
+            gtarray = gtdict[str(rep+1)][iix]
             sdpop1 = simData()
             gtpop1 = [''.join(str(n) for n in y) for y in gtarray]
             sdpop1.assign_sep(positions[rep], gtpop1)
-            #pspop1 = polySIM(sdpop1)
-            #stats
-            garudStats_t = (garudStats(sdpop1)) #garud 2015
-            garudH12_t.append(garudStats_t[0])
-            garudH1_t.append(garudStats_t[1])
-            garudH2_t.append(garudStats_t[2])
-            #lhaf_t = lhaf(sdpop1,1) #1-HAF is most common; Ronen 2016
-            #sfs
+            # pspop1 = polySIM(sdpop1)
+            # stats
+            garudStats_t = (garudStats(sdpop1))  # garud 2015
+            gH12t.append(garudStats_t['H12'])
+            gH1t.append(garudStats_t['H1'])
+            gH21t.append(garudStats_t['H2H1'])
+            # lhaf_t = lhaf(sdpop1,1) #1-HAF is most common; Ronen 2016
+            # sfs
             sfsarray, derived = site_freqspec_fx(gtarray, pos)
-            np.concatenate(sfs_t, sfsarray)
-            #hapconfig
-            hapconfig_t.append(haploconfig_fx(gtarray))
-            #derived
-            derivedfreq_t.append(derived)
-        garudH12.append([np.mean(pop) for pop in zip(*garudH12_t)])
-        garudH1.append([np.mean(pop) for pop in zip(*garudH1_t)])
-        garudH2.append([np.mean(pop) for pop in zip(*garudH2_t)])
-        sfs.append(np.sum(sfs_t)/sfs_t.shape[0])
-        #collapse all configs to common
-        hapconfig.append([(key,len(list(group))) for key, group in groupby(hapconfig_t)])
-        #msms
-        derivedfreq.append([np.mean(pop) for pop in zip(*derivedfreq_t)])
-        #jsfs
-        #jointsfs_fx(gtdict, popiix)
-    return(garudH12, garudH1, garudH2, sfs, hapconfig, derivedfreq)
+            sfst.append(sfsarray)
+            # hapconfig
+            hapt.append(haploconfig_fx(gtarray))
+            # derived
+            dfreqt.append(derived)
+        gH12.append(gH12t)
+        gH1.append(gH1t)
+        gH21.append(gH21t)
+        sfs.append(sfst)
+        hap.append(hapt)
+        dfreq.append(dfreqt)
+    garudH12 = [np.mean(pop) for pop in zip(*gH12)]
+    garudH1 = [np.mean(pop) for pop in zip(*gH1)]
+    garudH21 = [np.mean(pop) for pop in zip(*gH21)]
+    sfs_summ = [np.mean(pop, axis=0)/(popsizelist[i])
+                for i, pop in enumerate(zip(*sfs))]
+    hap_summ = [np.mean(pop, axis=0) for i, pop in enumerate(zip(*hap))]
+    haparray = [np.vstack(i) for i in zip(*hap)]
+    for config in haparray:
+        uniq, hapfreq = hapconfigfx(config)
+        hapconfig_common.append(zip(uniq, hapfreq))
+    derivedfreq = [np.mean(pop) for pop in zip(*dfreq)]
+    # jointsfs_fx(gtdict, popiix)
+    return(garudH12, garudH1, garudH21, sfs_summ, hap_summ, derivedfreq)
+
 
 def parse_msfile(msfiles_in, popsizelist, number_reps, priors):
-    '''calculates stats from ms files
+    """calculates stats from ms files
 
     Parameters
     ----------
@@ -165,7 +188,7 @@ def parse_msfile(msfiles_in, popsizelist, number_reps, priors):
         number of msfiles to loop, prefix of the msfiles, assumes
         "${}_{}.msout".format(prefix, int)
     popsizelist : list, int
-        population sizes for each subpop, if 
+        population sizes for each subpop, if
     number_reps : int
         number of reps in each file
     params : int
@@ -177,59 +200,69 @@ def parse_msfile(msfiles_in, popsizelist, number_reps, priors):
         array of positions
     genotype : array
         array of genotypess
-    '''
-    
+    """
+
     nind = sum(popsizelist)
     garudH12 = []
     garudH1 = []
-    garudH2 = []
+    garudH21 = []
     hapconfig_summary = []
     sfs = []
     origin_count = []
     derived = []
-    for params in range(priors): #for each file
-        positions = np.array([]).reshape(number_reps, 0)
+    positions = []
+    for params in range(priors):  # for each file
         gtdict = {}
         rep = 0
         origin = []
-        #read in msfile
-        with open("{}.{}.msout".format(msfiles_in, params), 'r') as msstyle:
-            #each ms-type file could have 1000s of reps
+        # read in msfile
+        with open("{}_{}.msout".format("out.sm", 1), 'r') as msstyle:
+            # each ms-type file could have 1000s of reps
             for line in msstyle:
-                line = line.decode('utf-8')                   
+                line = line.decode('utf-8')
                 if line.startswith("positions"):
-                    pos = np.array(line.strip().split()[1:],dtype=np.float64)
-                    np.concatenate(positions, pos)
+                    pos = np.array(line.strip().split()[1:], dtype=np.float64)
+                    positions.append(pos)
                     gt_array = np.zeros((nind, pos.shape[0]), dtype=np.uint8)
                     rep += 1
                     cix = 0
-                elif line.startswith("0") or line.startswith("1"): #by size of populations, add to gtdict
-                    line = line.decode('utf-8')
-                    hap = np.array(list(line.strip()), dtype=np.uint8)
-                    gt_array[cix, :] = hap
-                    cix += 1
-                elif line.startswith("Origin"): #if msms
+                    line = next(msstyle)
+                    try:
+                        while line.startswith("0") or line.startswith("1"):
+                            # by size of populations, add to gtdict
+                            line = line.decode('utf-8')
+                            hap = np.array(list(line.strip()), dtype=np.uint8)
+                            gt_array[cix, :] = hap
+                            cix += 1
+                            line = next(msstyle)
+                    except StopIteration:
+                        pass
+                    gtdict[str(rep)] = gt_array
+                elif line.startswith("Origin"):  # if msms
                     origin.append(int(line.strip().split(":")[1]))
-                else: 
+                else:
                     pass
-                gtdict[str(rep)] = gt_array
-        
-        #run file through stats
-        garudH12t, garudH1t, garudH2t, sfst, hapconfigt, derivedt = calc_otherstats(positions, gtdict, popsizelist)
-        #collect pop mean for ABC
-        garudH12.append(garudH12t)
-        garudH1.append(garudH1t)
-        garudH2.append(garudH2t)
-        sfs.append(sfst)
-        hapconfig_summary.append(hapconfigt)
-        #unused except for msms
-        origin_count.append(np.mean(origin))
-        derived.append(derivedt)
 
-    return(garudH12, garudH1, garudH2, sfs, hapconfig_summary, derived, origin_count)
+        # run file through stats
+        gH12t, gH1t, gH21t, sfst, hapt, dert = calc_otherstats(positions,
+                                                               gtdict,
+                                                               popsizelist)
+        # collect pop mean for ABC
+        garudH12.append(gH12t)  # extend?
+        garudH1.append(gH1t)
+        garudH21.append(gH21t)
+        sfs.append(sfst)
+        hapconfig_summary.append(hapt)
+        # unused except for msms
+        origin_count.append(np.mean(origin))
+        derived.append(dert)
+
+    return(garudH12, garudH1, garudH21, sfs, hapconfig_summary, derived,
+           origin_count)
+
 
 def parse_msstats(msstats_in, priors, demes):
-    '''parses file from msstats into pops and means values
+    """parses file from msstats into pops and means values
 
     Parameters
     ----------
@@ -244,23 +277,25 @@ def parse_msstats(msstats_in, priors, demes):
     Returns
     -------
 
-    '''
-    abcstats = np.zeros(priors * demes, 14) #some number
+    """
+    abcstats = np.zeros(priors * demes, 14)  # some number
     for f in range(priors):
-        msstat = pd.read_table("{}_{}".format(msstats_in, f), header=0, sep='\t')
+        msstat = pd.read_table("{}_{}".format(msstats_in, f), header=0,
+                               sep='\t')
         meanstat = msstat.groupby('pop').mean().values[2:]
-        #add to abcstats
+        # add to abcstats
         abcstats.append(meanstat)
     cols = msstat.columns[2:]
-    
+
     return(abcstats, cols)
 
+
 def format4ABC(priors_in, msfileprefix, popsizelist, number_reps):
-    '''creates file for R containing stats of ms or msms runs
-    
+    """creates file for R containing stats of ms or msms runs
+
     Parameters
     ----------
-    popsizelist : int, list 
+    popsizelist : int, list
         [10, 12, 15, 8, 10, 12] list of population sizes
     priors_in : file
         priors file
@@ -268,37 +303,38 @@ def format4ABC(priors_in, msfileprefix, popsizelist, number_reps):
         files prefix for ms and msstats
     number_reps : int
         number of reps in each ms file
-        
+
     Returns
     -------
     ABCstatTable : pandas df object
-    '''
-    priors = pd.read_table("{}".format(priors_in),header=0, sep='\t') 
+    """
+    priors = pd.read_table("{}".format(priors_in), header=0, sep='\t')
     lenpriors = priors.shape[0]
     demes = len(popsizelist)
-    garudH12, garudH1, garudH2, sfs, hapconfig_summary, derived, origincount = parse_msfile(msfileprefix, 
-                                                                                             popsizelist, 
-                                                                                             number_reps, 
-                                                                                             lenpriors)
+    gH12, gH1, gH21, sfs, hap, der, orig = parse_msfile(msfileprefix,
+                                                        popsizelist,
+                                                        number_reps,
+                                                        lenpriors)
     abcstats, cols = parse_msstats(msfileprefix, lenpriors, demes)
     priors = ["theta", "selection"]
-    names = priors + "pop" + cols + ["garudH12", "garudH1", "garudH2", "sfs", "hapconfig", "derived", "origins"]
-    pop = range(1,demes + 1) * lenpriors
-    origin = np.repeat(origincount, demes)
-      
+    names = priors + "pop" + cols + ["garudH12", "garudH1", "garudH21", "sfs",
+                                     "hapconfig", "derived", "origins"]
+    pop = range(1, demes + 1) * lenpriors
+    origin = np.repeat(orig, demes)
+
     ABCstatTable = pd.DataFrame({np.repeat(priors.theta.values, demes),
                                  pop,
                                  abcstats,
-                                 garudH12,
-                                 garudH1,
-                                 garudH2,
+                                 gH12,
+                                 gH1,
+                                 gH21,
                                  sfs,
-                                 hapconfig_summary,
-                                 derived,
-                                 origincount
+                                 hap,
+                                 der,
+                                 origin
                                  })
     ABCstatTable.to_csv("ABCstatTable.csv")
     return(None)
-    
+
 if __name__ == '__main__':
-    format4ABC(args.priors, args.INmsfile, args.populations, args.reps)    
+    format4ABC(args.priors, args.INmsfile, args.populations, args.reps)
